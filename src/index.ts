5eb8d66aaf794ab960138a0c801cfd722a5396ae
DavidSheltonSF <david.faria012@gmail.com>
2026-02-14 22:56:49 -0300

    refactor(router): move all products-related routes to routs/products.ts

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,28 @@
-import express, {type Request, type Response} from 'express';
+import express, { type Request, type Response } from 'express';
+import { PostgreHelper } from './database/database';
+import cookieParser from 'cookie-parser';
+import { configRouter } from './routes/router';
 
-const app = express();
+export const app = express();
+app.use(express.json());
+app.use(cookieParser());
+app.use(configRouter());
 const port = 3001;
+export const dbConnection = PostgreHelper.getInstance();
 
-app.get("/", (req: Request, resp: Response) => {
-  return resp.json("API is working");
-})
+(async () => {
+  try {
+    await dbConnection.connect();
+    console.log('Connected');
+  } catch (error: any) {
+    console.log(error);
+  }
 
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`)
-})
+  app.get('/', (req: Request, res: Response) => {
+    return res.json('API is working');
+  });
+
+  app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+  });
+})();
---
+++ src/routes/products.ts
@@ -0,0 +1,64 @@
+import {Router, type Request, type Response } from "express";
+import { dbConnection } from "..";
+
+export function configProductsRoutes(router: Router){
+  router.post('/products', async (req: Request, res: Response) => {
+    if (!req.body) {
+      return res.status(400).json({
+        message: 'Missing request body',
+      });
+    }
+
+    const { code, name, price } = req.body;
+    if (!code || !name || !price) {
+      return res.status(400).json({
+        message: 'Missing required fields',
+      });
+    }
+
+    const query = {
+      text: `INSERT INTO product(code, name, price) VALUES($1, $2, $3) RETURNING *`,
+      values: [code, name, price],
+    };
+    const result = await dbConnection.query(query);
+    console.log(result);
+
+    return res.status(201).json({
+      data: result.rows[0],
+    });
+  });
+
+  router.put('/products/:id', async (req: Request, res: Response) => {
+    const productId = req.params.id;
+
+    if (!productId) {
+      return res.status(400).json({
+        message: 'Missing product id',
+      });
+    }
+
+    if (!req.body) {
+      return res.status(400).json({
+        message: 'Missing request body',
+      });
+    }
+
+    const { code, name, price } = req.body;
+    if (!code || !name || !price) {
+      return res.status(400).json({
+        message: 'Missing required fields',
+      });
+    }
+
+    const query = {
+      text: `UPDATE product SET code =  $1, name = $2, price = $3 WHERE id = ${productId}`,
+      values: [code, name, price],
+    };
+
+    await dbConnection.query(query);
+
+    return res.status(200).json({
+      message: 'updated',
+    });
+  });
+}
