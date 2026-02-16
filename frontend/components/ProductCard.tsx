8d6acb74365d7bf3c9b0653b9119b44f0945ac13
DavidSheltonSF <david.faria012@gmail.com>
2026-02-16 19:41:49 -0300

    feat(components): add ProductCard

---
+++ frontend/components/ProductCard.tsx
@@ -0,0 +1,31 @@
+import { EditIcon } from './icons/EditIcon';
+import { TrashIcon } from './icons/TrashIcon';
+import { LabelValue } from './LabelValue';
+
+interface Props {
+  id?: number;
+  code: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+export function ProductCard({ code, name, price, quantity }: Props) {
+  return (
+    <article className="flex flex-col justify-between w-full h-[216px] h-[100px]  bg-white border border-black/80 p-[16px] rounded-lg">
+      <header className="flex justify-between">
+        <h1 className="text-2xl">{name}</h1>
+        <div className="flex gap-[16px]">
+          <EditIcon width="32px" height="32px" />
+          <TrashIcon width="32px" height="32px" />
+        </div>
+      </header>
+      <div className="flex flex-col gap-[8px] text-lg">
+        <LabelValue label="code" value={code} />
+        <LabelValue label="name" value={name} />
+        <LabelValue label="price" value={price} />
+        <LabelValue label="quantity" value={quantity} />
+      </div>
+    </article>
+  );
+}
