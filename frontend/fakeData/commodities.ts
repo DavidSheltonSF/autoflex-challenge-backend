2325c14431d71d67ca334093546ffe09ea6eb349
DavidSheltonSF <david.faria012@gmail.com>
2026-02-16 20:03:17 -0300

    feat(fakeData): add commodities

---
+++ frontend/fakeData/commodities.ts
@@ -0,0 +1,31 @@
+export const commodities = [
+  {
+    id: 1,
+    code: 'comod1',
+    name: 'perna-de-três',
+    price: 40.5,
+    quantity: 200,
+  },
+  {
+    id: 2,
+    code: 'comod2',
+    name: 'parafuso',
+    price: 0.25,
+    quantity: 800,
+  },
+  {
+    id: 3,
+    code: 'comod3',
+    name: 'tábua de madeira',
+    price: 180.5,
+    quantity: 20,
+  },
+
+  {
+    id: 4,
+    code: 'comod4',
+    name: 'maçaneta',
+    price: 20.5,
+    quantity: 7,
+  },
+];
