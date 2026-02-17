9a9a9cc7b255164a1635e541a2b95144c71ad94c
DavidSheltonSF <david.faria012@gmail.com>
2026-02-17 18:12:07 -0300

    feat(frontend/services): add fetchAddProduct

---
+++ frontend/services/fetchAddProduct.ts
@@ -0,0 +1,23 @@
+export async function fetchAddProduct(formData: FormData) {
+  const code = formData.get('code');
+  const name = formData.get('name');
+  const price = formData.get('price');
+  const quantity = formData.get('quantity');
+
+  const response = await fetch('http://localhost:3002/products', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ code, name, price, quantity }),
+  });
+
+  if (!response.ok) {
+    const errorText = await response.text();
+    console.log(errorText);
+    throw Error(errorText);
+  }
+
+  const json = await response.json();
+  return json.data;
+}
