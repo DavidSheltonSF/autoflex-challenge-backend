e071dc08a26e2c916b406b36441812f7a69b9c79
DavidSheltonSF <david.faria012@gmail.com>
2026-02-17 18:49:40 -0300

    feat(components): add ProductCardSkeleton

---
+++ frontend/components/ProductCardSkeleton.tsx
@@ -0,0 +1,22 @@
+import { EditIcon } from './icons/EditIcon';
+import { TrashIcon } from './icons/TrashIcon';
+
+export function ProductCardSkelton() {
+  return (
+    <article className="flex flex-col gap-[16px] w-full bg-white border border-black/80 p-[16px] rounded-lg">
+      <header className="flex justify-between">
+        <span className="h-[32px] w-[100px] bg-gray-300  animate-pulse"></span>
+        <div className="flex gap-[16px]">
+          <EditIcon width="32px" height="32px" />
+          <TrashIcon width="32px" height="32px" />
+        </div>
+      </header>
+      <div className="flex flex-col gap-[8px] text-lg">
+        <span className="h-[20px] w-[120px] bg-gray-300 animate-pulse"></span>
+        <span className="h-[20px] w-[100px] bg-gray-300 animate-pulse"></span>
+        <span className="h-[20px] w-[130px] bg-gray-300 animate-pulse"></span>
+        <span className="h-[20px] w-[110px] bg-gray-300 animate-pulse"></span>
+      </div>
+    </article>
+  );
+}
