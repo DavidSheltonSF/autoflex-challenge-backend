6f75838a872f319f4ab2306ab8dfa7e6837c9332
DavidSheltonSF <david.faria012@gmail.com>
2026-02-17 18:14:23 -0300

    refactor(ProductsList): use AddButton and AddProductModal

--- frontend/components/ProductsList.tsx
+++ frontend/components/ProductsList.tsx
@@ -1,5 +1,11 @@
+'use client';
 import { Product } from '@/types/Product';
 import { ProductCard } from './ProductCard';
+import { AddButton } from './AddButton';
+import { BaseModal } from './modals/BaseModal';
+import { AddProductModalContext } from '@/contexts/AddProductModalContext';
+import { useContext } from 'react';
+import { AddProductModal } from './modals/AddProductModal';
 
 interface Props {
   products: Product[];
@@ -10,5 +16,24 @@ export function ProductsList({ products }: Props) {
     return <ProductCard key={index} {...product} />;
   });
 
-  return <div className="flex flex-col gap-[24px] w-full">{renderProducts}</div>;
+  const context = useContext(AddProductModalContext);
+  if(!context){
+    throw Error('Missing AddProductModalContext');
+  }
+
+  const {isOpen, setIsOpen} = context;
+
+  return (
+    <div className="flex flex-col gap-[24px] w-full">
+      <AddButton
+        label="Add new Product"
+        width="180px"
+        onClick={() => {
+          setIsOpen(!isOpen)
+        }}
+      />
+      <AddProductModal/>
+      <div>{renderProducts}</div>
+    </div>
+  );
 }
