42ee9e58a3b7e61133cbdbd0677a771df0a19d62
DavidSheltonSF <david.faria012@gmail.com>
2026-02-18 19:33:04 -0300

    chore(fetchUpdateProducts): remove quantity field

--- frontend/services/fetchUpdateProduct.ts
+++ frontend/services/fetchUpdateProduct.ts
@@ -2,14 +2,13 @@ export async function fetchUpdateProduct(id: string, formData: FormData) {
   const code = formData.get('code');
   const name = formData.get('name');
   const price = formData.get('price');
-  const quantity = formData.get('quantity');
 
   const response = await fetch(`http://localhost:3002/products/${id}`, {
     method: 'PUT',
     headers: {
       'Content-Type': 'application/json',
     },
-    body: JSON.stringify({ code, name, price, quantity }),
+    body: JSON.stringify({ code, name, price }),
   });
 
   if (!response.ok) {
