bf378baf7dd27accd7e7cf8d0570a0423eb90761
DavidSheltonSF <david.faria012@gmail.com>
2026-02-18 15:37:12 -0300

    feat(components): add UpdateProductModal and add it to the global layout

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from 'next/font/google';
 import './styles/globals.css';
 import './styles/animations.css';
 import { ModalsProvider } from '@/contexts/ModalsProvider';
+import { UpdateProductModal } from '@/components/modals/UpdateProductModal';
 
 const geistSans = Geist({
   variable: '--font-geist-sans',
@@ -27,7 +28,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-        <ModalsProvider>{children}</ModalsProvider>
+        <ModalsProvider>
+          <UpdateProductModal />
+          <div>{children}</div>
+        </ModalsProvider>
       </body>
     </html>
   );
---
+++ frontend/components/modals/UpdateProductModal.tsx
@@ -0,0 +1,90 @@
+'use client';
+import { BaseModal } from './BaseModal';
+import { useContext, useEffect, useState } from 'react';
+import { FechingState } from '@/types/FechingState';
+import { ProductForm } from '../ProductForm';
+import { UpdateProductModalContext } from '@/contexts/UpdateProductModalContext';
+import { Product } from '@/types/Product';
+import { fetchProductById } from '@/services/fetchProductById';
+import { RequestStatus } from '@/types/RequestStatus';
+import { fetchUpdateProduct } from '@/services/fetchUpdateProduct';
+
+export function UpdateProductModal() {
+  const [fetchProductState, setFetchProductState] = useState<FechingState<Product> | null>(null);
+
+  const context = useContext(UpdateProductModalContext);
+  if (!context) {
+    throw Error('Missing UpdateProductModalContext');
+  }
+  const { isOpen, setIsOpen, productId } = context;
+
+  useEffect(() => {
+    function cleanStates() {
+      setFetchProductState(null);
+      setFetchProductState(null);
+    }
+
+    async function getProductById() {
+      try {
+        setFetchProductState({ status: RequestStatus.loading });
+        const product = await fetchProductById(productId || '');
+        await new Promise((resolve) => setTimeout(resolve, 3000));
+        setFetchProductState({ status: RequestStatus.ok, data: product });
+      } catch (error) {
+        console.log(error);
+        setFetchProductState({ status: RequestStatus.error, message: 'Something went wrong' });
+      }
+    }
+
+    if (!isOpen) {
+      cleanStates();
+      return;
+    }
+
+    getProductById();
+  }, [isOpen]);
+
+  async function handleSubmit(formData: FormData) {
+    try {
+      setFetchProductState({ status: RequestStatus.loading });
+      const updatedProduct = await fetchUpdateProduct(productId || '', formData);
+      setFetchProductState({
+        status: RequestStatus.ok,
+        data: updatedProduct,
+        message: 'Product updated succesfuly',
+      });
+    } catch (error) {
+      console.log(error);
+      setFetchProductState({ status: RequestStatus.error, message: 'Something went wrong' });
+    }
+  }
+
+  const productIsLoading = fetchProductState?.status === RequestStatus.loading;
+
+  return (
+    isOpen && (
+      <BaseModal
+        isLoading={productIsLoading}
+        close={() => setIsOpen(false)}
+        additionalStyles="h-auto w-[80vw] min-lg:w-[400px]"
+      >
+        <div className="flex flex-col gap-[16px] justify-center size-full">
+          {fetchProductState && (
+            <span className="w-full text-center font-bold">
+              <p
+                className={fetchProductState.status === 'error' ? 'text-red-500' : 'text-green-500'}
+              >
+                {fetchProductState.message}
+              </p>
+            </span>
+          )}
+          <ProductForm
+            data={fetchProductState?.data}
+            formAction={handleSubmit}
+            buttonLabel="Add Product"
+          />
+        </div>
+      </BaseModal>
+    )
+  );
+}
