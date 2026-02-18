3979c5f2f4508655239e4f2d8b1c2a5cb4973994
DavidSheltonSF <david.faria012@gmail.com>
2026-02-18 19:41:44 -0300

    refactor(CommoditiesSection): fetch commodities from the API

--- frontend/components/dynamicSections/CommoditiesSection.tsx
+++ frontend/components/dynamicSections/CommoditiesSection.tsx
@@ -1,12 +1,39 @@
+'use client';
+
+import { useEffect, useState } from 'react';
 import { CommoditiesList } from '../CommoditiesList';
-import { commodities } from '@/fakeData/commodities';
+import { fetchCommodities } from '@/services/fetchCommodities';
+import { Commodity } from '@/types/Commodity';
+import { FechingState } from '@/types/FechingState';
+import { RequestStatus } from '@/types/RequestStatus';
+import { WithId } from '@/types/WithId';
 import { SearchBar } from '../SearchBar';
 
 export function CommoditiesSection() {
+  const [fetching, setFeching] = useState<FechingState<WithId<Commodity>[]>>({
+    status: RequestStatus.loading,
+    data: [],
+  });
+
+  const isLoading = fetching.status === RequestStatus.loading ? true : false;
+
+  useEffect(() => {
+    async function loadCommodities() {
+      try {
+        setFeching({ status: RequestStatus.loading });
+        const Commodities = await fetchCommodities();
+        setFeching({ status: RequestStatus.ok, data: Commodities });
+      } catch (error) {
+        console.log(error);
+        setFeching({ status: RequestStatus.error });
+      }
+    }
+    loadCommodities();
+  }, []);
   return (
     <div className="flex flex-col gap-[24px]">
       <SearchBar />
-      <CommoditiesList commodities={commodities} />
+      <CommoditiesList commodities={fetching.data || []} isLoading={isLoading} />
     </div>
   );
 }
