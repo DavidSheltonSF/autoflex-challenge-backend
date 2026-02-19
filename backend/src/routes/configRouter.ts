3bb54db16f2979feb4e387b5a05868c478428363
DavidSheltonSF <david.faria012@gmail.com>
2026-02-19 20:13:04 -0300

    chore(routes): remove unused productsCommodities routes

--- backend/src/routes/configRouter.ts
+++ backend/src/routes/configRouter.ts
@@ -1,12 +1,10 @@
 import { Router } from 'express';
 import { configProductsRoutes } from './products';
 import { configCommoditiesRoutes } from './commodities';
-import { configproductsCommoditiesRoutes } from './productsCommodities';
 
 export function configRouter(): Router {
   const router = Router();
   configProductsRoutes(router);
   configCommoditiesRoutes(router);
-  configproductsCommoditiesRoutes(router);
   return router;
 }
