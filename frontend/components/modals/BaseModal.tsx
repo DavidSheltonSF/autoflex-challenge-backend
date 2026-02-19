6b72897f43f64c4136dd96152f577032eecf9c00
DavidSheltonSF <david.faria012@gmail.com>
2026-02-19 16:55:46 -0300

    chore(BaseModal):  enhance design and user experience
    -  Add a detail with primary-color
    -  Add a background color transition to the close button

--- frontend/components/modals/BaseModal.tsx
+++ frontend/components/modals/BaseModal.tsx
@@ -24,18 +24,18 @@ export function BaseModal({ close, children, additionalStyles, isLoading }: Prop
       ref={modalRef}
       className={`flex flex-col fixed top-[10%] left-[50%] translate-x-[-50%] bg-color-white shadow-[0_0_3px_black] rounded-lg fade-in-animation ${additionalStyles} min-h-[20vh]`}
     >
-      <div className="flex justify-end items-center p-[8px] border-b">
+      <div className="flex justify-end items-center p-[8px] border-b bg-color-primary">
         <button
-          className="rounded-sm bg-color-white hover:brightness-96 cursor-pointer"
+          className="rounded-sm hover:bg-[var(--color-white)]/30 transition-[background] duration-300 cursor-pointer"
           onClick={handleCloseModal}
         >
-          <CloseIcon width="32px" height="32px" />
+          <CloseIcon width="32px" height="32px" color="var(--color-white)" />
         </button>
       </div>
       {isLoading ? (
-        <span className='m-auto animate-spin'>
+        <span className="m-auto animate-spin">
           {' '}
-          <LoadingIcon height="56px" width='56px' />
+          <LoadingIcon height="56px" width="56px" />
         </span>
       ) : (
         <div className="flex size-full p-[16px] fade-in-animation">{children}</div>
