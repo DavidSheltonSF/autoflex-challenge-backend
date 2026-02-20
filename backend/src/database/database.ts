4c06a5cb50174774149ff8e00be292fab3d816c1
DavidSheltonSF <david.faria012@gmail.com>
2026-02-20 02:03:26 -0300

    refactor(database): get database credentials from the environment

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,45 +1,46 @@
-import { Client, ClientConfig, QueryResult } from "pg";
+import { Client, ClientConfig, QueryResult } from 'pg';
+import { config } from 'dotenv';
+config();
 
 export class PostgreHelper {
   static instance: PostgreHelper | null = null;
   private client: Client | null = null;
 
-  private constructor(config: ClientConfig){
-     this.client = new Client(config)
+  private constructor(config: ClientConfig) {
+    this.client = new Client(config);
   }
 
   static getInstance(): PostgreHelper {
-    if(this.instance) return this.instance;
-    
-    this.instance =  new PostgreHelper({
-      host: "localhost",
-      user: "postgres",
-      port: 5432,
-      password: "admin",
-      database: "AutoflexChallenge"
+    if (this.instance) return this.instance;
+    this.instance = new PostgreHelper({
+      host: process.env.DATABASE_HOST,
+      user: process.env.DATABASE_USER,
+      port: Number(process.env.DATABASE_PORT),
+      password: process.env.DATABASE_PASSWORD,
+      database: process.env.DATABASE,
     });
 
     return this.instance;
   }
 
-  async connect(){
-    if(!this.client) {
-      throw Error("Could not connect because PostgreHelper.client is null");
-    };
+  async connect() {
+    if (!this.client) {
+      throw Error('Could not connect because PostgreHelper.client is null');
+    }
     await this.client.connect();
   }
 
-  async query(query: any): Promise<QueryResult<any>>{
-    if(!this.client) {
-      throw Error("Could not make query because PostgreHelper.client is null");
-    };
+  async query(query: any): Promise<QueryResult<any>> {
+    if (!this.client) {
+      throw Error('Could not make query because PostgreHelper.client is null');
+    }
     return await this.client.query(query);
   }
 
-  async disconnect(){
-    if(!this.client) {
-      throw Error("Could not disconnect because PostgreHelper.client is null");
-    };
+  async disconnect() {
+    if (!this.client) {
+      throw Error('Could not disconnect because PostgreHelper.client is null');
+    }
     await this.client.end();
     this.client = null;
   }
