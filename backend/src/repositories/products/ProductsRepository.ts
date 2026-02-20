68221c70df250d0cf6d4a5c8acd8e99e5cf727ed
DavidSheltonSF <david.faria012@gmail.com>
2026-02-20 05:52:24 -0300

    fix(ProductsRepository): fix findAll and findById types

--- backend/src/repositories/products/ProductsRepository.ts
+++ backend/src/repositories/products/ProductsRepository.ts
@@ -2,8 +2,8 @@ import { Product } from '../../types/Product';
 import { WithId } from '../../types/WithId';
 
 export interface ProductsRepository {
-  findAll: () => Promise<WithId<Product[]>>;
-  findById: () => Promise<WithId<Product>>;
+  findAll: () => Promise<WithId<Product>[]>;
+  findById: (id: string) => Promise<WithId<Product>>;
   create: (product: Product) => Promise<WithId<Product>>;
   update: (id: string, product: Product) => Promise<WithId<Product>>;
   delete: (id: string) => Promise<WithId<Product>>;
