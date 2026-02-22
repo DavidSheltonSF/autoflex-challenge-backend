8cd3256b3bdd9f4547470b3641725e2ffaaa5a01
DavidSheltonSF <david.faria012@gmail.com>
2026-02-22 11:27:37 -0300

    feat(controllers/types): add HttpStatusCodeStr enum

---
+++ backend/src/controllers/types/HttpStatusCodeSting.ts
@@ -0,0 +1,8 @@
+export enum HttpStatusCodeStr {
+  ok = 'OK',
+  badRequest = 'BAD_REQUEST',
+  unauthorized = 'UNAUTHORIZED',
+  notFound = 'NOT_FOUND',
+  unprocessableEntity = 'UNPROCESSABLE_ENTITY',
+  serverError = 'SERVER_ERROR',
+}
