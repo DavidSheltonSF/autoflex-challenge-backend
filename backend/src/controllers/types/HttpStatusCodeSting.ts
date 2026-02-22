dca7dc125bc04fa31011ec328c0a96dffd3cdce0
DavidSheltonSF <david.faria012@gmail.com>
2026-02-22 12:31:46 -0300

    chore(controllers/types): remove unnecessary HttpStatusCodeStr

--- backend/src/controllers/types/HttpStatusCodeSting.ts
+++
@@ -1,8 +0,0 @@
-export enum HttpStatusCodeStr {
-  ok = 'OK',
-  badRequest = 'BAD_REQUEST',
-  unauthorized = 'UNAUTHORIZED',
-  notFound = 'NOT_FOUND',
-  unprocessableEntity = 'UNPROCESSABLE_ENTITY',
-  serverError = 'SERVER_ERROR',
-}
