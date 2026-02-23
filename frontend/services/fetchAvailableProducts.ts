574a909b286c11d6ef7b8f157bfd10509552cc9e
DavidSheltonSF <david.faria012@gmail.com>
2026-02-23 00:37:21 -0300

    refactor(frontend): type fetchAvailableProducts response with ProductsAndCommodity

--- frontend/services/fetchAvailableProducts.ts
+++ frontend/services/fetchAvailableProducts.ts
@@ -1,8 +1,9 @@
 import { API_URL } from '@/config/api';
 import { Product } from '@/types/Product';
+import { ProductAndCommodity } from '@/types/ProductAndCommodity';
 import { WithId } from '@/types/WithId';
 
-export async function fetchAvailableProducts(): Promise<WithId<Product & { quantity: number }>[]> {
+export async function fetchAvailableProducts(): Promise<ProductAndCommodity[]> {
   const response = await fetch(`${API_URL}/availableProducts`);
 
   if (!response.ok) {
