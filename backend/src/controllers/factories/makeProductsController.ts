5a85fd578b0779993e2eedb1bb81b0e7f168ee72
DavidSheltonSF <david.faria012@gmail.com>
2026-02-23 00:14:49 -0300

    refactor(makeProductsController): add ProductsCommoditiesRepository to ProductService for maching recent changes

--- backend/src/controllers/factories/makeProductsController.ts
+++ backend/src/controllers/factories/makeProductsController.ts
@@ -1,5 +1,6 @@
 import { PostgresCommoditiesRepository } from '../../repositories/commodities/PostgresCommoditiesRepository';
 import { PostgresProductsRepository } from '../../repositories/products/PostgresProductsRepository';
+import { PostgresProductsCommoditiesRepository } from '../../repositories/productsCommoditiesRelation/PostgresProductsCommoditiesRepository';
 import { CommoditiesService } from '../../services/commodities/CommoditiesService';
 import { ProductService } from '../../services/products/ProductService';
 import { IProductsController } from '../products/IProductsController';
@@ -8,7 +9,8 @@ import { ProductsController } from '../products/ProductsController';
 export function makeProductsController(): IProductsController {
   const productsRepository = new PostgresProductsRepository();
   const commoditiesRepository = new PostgresCommoditiesRepository();
-  const productsService = new ProductService(productsRepository);
+  const productsCommoditiesRepository = new PostgresProductsCommoditiesRepository();
+  const productsService = new ProductService(productsRepository, productsCommoditiesRepository);
   const commoditiesService = new CommoditiesService(commoditiesRepository);
   const productsController = new ProductsController(productsService, commoditiesService);
   return productsController;
