5886ca007bd48cf3370c6b4edf88d0e975e16bad
DavidSheltonSF <david.faria012@gmail.com>
2026-02-23 00:38:17 -0300

    refactor(routes/products): use ProductsController in /availableProducts route

--- backend/src/routes/products.ts
+++ backend/src/routes/products.ts
@@ -1,6 +1,4 @@
-import { Router, type Request, type Response } from 'express';
-import { dbConnection } from '..';
-import { getViableProducts } from '../utils/getViableProducts';
+import { Router } from 'express';
 import { expressHttpAdapter } from './adapters/expressHttpAdapter';
 import { makeProductsController } from '../controllers/factories/makeProductsController';
 
@@ -21,32 +19,5 @@ export function configProductsRoutes(router: Router) {
     '/products/:productId/commodities/:commodityId',
     expressHttpAdapter(productsController.removeCommodity)
   );
-
-  router.get('/availableProducts', async (req: Request, res: Response) => {
-    const query = `
-  SELECT p.id, p.name, p.price, pc.quantity as commodityQuantity, c.name as commodityName, c.quantity as availableCommodityQuantity
-  FROM products p
-  INNER JOIN products_commodities pc ON p.id = pc.productid
-  INNER JOIN commodities c ON c.id = pc.commodityid
-   `;
-    const result = await dbConnection.query(query);
-    const rows = result.rows;
-    let groupedRowsByProductIdObj: Record<string, any[]> = {};
-    rows.forEach((row) => {
-      if (groupedRowsByProductIdObj[row.id] === undefined) {
-        groupedRowsByProductIdObj[row.id] = [];
-      }
-      groupedRowsByProductIdObj[row.id]?.push(row);
-    });
-
-    const groupedRowsByProductId = Object.values(groupedRowsByProductIdObj);
-
-    const viableProducts = getViableProducts(groupedRowsByProductId);
-
-    return res.status(200).json({
-      data: viableProducts,
-    });
-  });
-
-  
+  router.get('/availableProducts', expressHttpAdapter(productsController.findAllAvailableProducts));
 }
