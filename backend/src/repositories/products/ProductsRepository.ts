f4d92d76cc8dc806bbe06b04e95cf469c371e285
DavidSheltonSF <david.faria012@gmail.com>
2026-02-23 00:22:25 -0300

    refactor(ProductRepository): remove unnecessary addCommodity, findAllCommodities and removeCommodity, since ProductsCommoditiesRepository already have similar methods

--- backend/src/repositories/products/ProductsRepository.ts
+++ backend/src/repositories/products/ProductsRepository.ts
@@ -1,6 +1,4 @@
-import { Commodity } from '../../types/Commodity';
 import { Product } from '../../types/Product';
-import { ProductCommodityRelation } from '../../types/ProductCommodityRelation';
 import { WithId } from '../../types/WithId';
 
 export interface ProductsRepository {
@@ -10,12 +8,5 @@ export interface ProductsRepository {
   updateById: (id: string, product: Product) => Promise<WithId<Product>>;
   deleteById: (id: string) => Promise<WithId<Product>>;
   checkExistence: (id: string) => Promise<boolean>;
-  findAllCommodities: (productId: string) => Promise<WithId<Commodity>[]>;
-  addCommodity: (
-    productCommodityRelation: ProductCommodityRelation
-  ) => Promise<WithId<ProductCommodityRelation>>;
-  removeCommodity: (
-    productId: string,
-    commodityId: string
-  ) => Promise<WithId<ProductCommodityRelation>>;
+
 }
